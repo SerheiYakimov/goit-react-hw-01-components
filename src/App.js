228a342cb26c1b0d38001f057952742a9cbee4f7
SerheiYakimov <ysergey2901@gmail.com>
2021-10-06 00:19:46 +0300

    add Profile

---
+++ src/App.js
@@ -0,0 +1,23 @@
+
+import './App.css';
+import { Profile } from './components/Profile/Profile';
+import user from './user.json'
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Profile
+      name={user.name}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      stats={user.stats}/>
+    </div>
+  );
+}
+
+export default App;
+
+
